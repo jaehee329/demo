cc62bb6d6657773b6f0d302836f6efc55cf3f14b
jaehee329 <jjhs9803@gmail.com>
2022-12-05 22:01:05 +0900

    add lombok, sample RestController

---
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+// @SpringBootTest
+@WebMvcTest
+class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	@DisplayName("hello가 리턴되어야 한다")
+	public void helloTest() throws Exception {
+		mockMvc.perform(get("/hello")).andExpectAll(
+			status().isOk(),
+			content().string("hello"));
+	}
+
+	@Test
+	@DisplayName("HelloResponseDto가 리턴되어야 한다")
+	public void helloDtoTest() throws Exception {
+		String name = "kim";
+		int amount = 1000;
+
+		mockMvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name", is(name)))
+			.andExpect(jsonPath("$.amount", is(amount)));
+	}
+}
