317f51dcff2dfcdc3cdde5cb42ecb382d4782264
jaehee329 <jaehee329@yonsei.ac.kr>
2022-12-22 00:22:38 +0900

    add Posts get

--- src/main/java/com/example/community/controller/PostsApiController.java
+++ src/main/java/com/example/community/controller/PostsApiController.java
@@ -1,9 +1,12 @@
 package com.example.community.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.community.controller.dto.PostsResponseDto;
 import com.example.community.controller.dto.PostsSaveRequestDto;
 import com.example.community.service.PostsService;
 
@@ -14,6 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class PostsApiController {
 
 	private final PostsService postsService;
+
+	@GetMapping("/api/v1/posts/{id}")
+	public PostsResponseDto findById(@PathVariable Long id) {
+		return postsService.findById(id);
+	}
 
 	@PostMapping("/api/v1/posts")
 	public Long save(@RequestBody PostsSaveRequestDto postsSaveRequestDto) {
---
+++ src/main/java/com/example/community/controller/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.community.controller.dto;
+
+import com.example.community.domain.posts.Posts;
+
+public class PostsResponseDto {
+
+	private Long id;
+	private String title;
+	private String content;
+	private String author;
+
+	public PostsResponseDto(Posts post) {
+		this.id = post.getId();
+		this.title = post.getTitle();
+		this.content = post.getContent();
+		this.author = post.getAuthor();
+	}
+}
