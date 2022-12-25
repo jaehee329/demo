b3cb703c5fa5af205cb5e08f835a6ce428144d54
jaehee329 <jaehee329@yonsei.ac.kr>
2022-12-26 00:31:45 +0900

    Test(PostsApiController): add Posts update test

--- src/main/java/com/example/community/service/PostsService.java
+++ src/main/java/com/example/community/service/PostsService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.community.controller.dto.PostsResponseDto;
 import com.example.community.controller.dto.PostsSaveRequestDto;
+import com.example.community.controller.dto.PostsUpdateRequestDto;
 import com.example.community.domain.posts.Posts;
 import com.example.community.domain.posts.PostsRepository;
 
@@ -16,12 +17,21 @@ public class PostsService {
 	private final PostsRepository postsRepository;
 
 	public PostsResponseDto findById(Long id) {
-		Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. 게시글 id = " + id));
+		Posts post = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. 게시글 id = " + id));
 		return new PostsResponseDto(post);
 	}
 
 	@Transactional
 	public Long save(PostsSaveRequestDto requestDto) {
 		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. 게시글 id = " + id));
+		posts.update(requestDto.getTitle(), requestDto.getContent(), requestDto.getAuthor());
+		return id;
 	}
 }
