88c0e9662d8099204091c92732f6b64ce1046cf1
jaehee329 <jjhs9803@gmail.com>
2023-01-15 23:26:53 +0900

    change session db from embedded tomcat to JDBC

--- src/main/java/com/example/community/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/community/config/auth/dto/SessionUser.java
@@ -1,11 +1,13 @@
 package com.example.community.config.auth.dto;
 
+import java.io.Serializable;
+
 import com.example.community.domain.user.Users;
 
 import lombok.Getter;
 
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
 	private String name;
 	private String email;
